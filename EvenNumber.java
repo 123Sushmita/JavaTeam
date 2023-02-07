6b98e58ff50e4ad10f89c2af23500cef36b59f46
123Sushmita <124117849+123Sushmita@users.noreply.github.com>
2023-02-06 22:17:59 -0600

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,14 @@
+public class EvenNumber {
+    public static void main(String[] args) {
+        int n =30;
+        int i = 3;
+        System.out.println("Even number from 3 to 30:"+".");
+        for (i =3; i<=n ;i++)
+        {
+            if (i%2 == 0)
+                System.out.println(i + "");
+    }
+
+    }
+
+}

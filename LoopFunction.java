29c0f1523b746e4ff9070f88ecebb312de53ecaf
123Sushmita <124117849+123Sushmita@users.noreply.github.com>
2023-02-06 20:46:29 -0600

    Add files via upload

---
+++ LoopFunction.java
@@ -0,0 +1,12 @@
+public class LoopFunction {
+    public static void main(String[] args) {
+        System.out.println("Begin with main");
+        int x= 0;
+        while(x <= 99){
+            System.out.println("this is a line...."+x);
+            x=x+1;
+            System.out.println("end ");
+        }
+
+    }
+}

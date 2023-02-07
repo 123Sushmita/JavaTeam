0ff72b1a5b92fb1a2c5b6c91e7160dfa4dddbc02
123Sushmita <124117849+123Sushmita@users.noreply.github.com>
2023-02-06 22:23:00 -0600

    Add files via upload

---
+++ OddNumber.java
@@ -0,0 +1,15 @@
+public class OddNumber {
+    public static void main(String[] args) {
+        int n =20;
+                int i = 2;
+                System.out.println("Odd number from 2 to 20:"+".");
+                for (i =2; i<=n ;i++)
+                {
+                    if (i%2 == 1)
+                        System.out.println(i + "");
+                }
+
+            }
+
+        }
+
